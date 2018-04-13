62c2ac4a6d2a21d250701c9d813625554478bf82
liuyuancheng <tenstone@foxmail.com>
2018-04-13 18:22:22 +0800

    实现自动重载

--- app/resources/user.go
+++ app/resources/user.go
@@ -11,8 +11,9 @@ type UserResource struct {
 
 func (r *UserResource) FetchAllUsers(c *gin.Context) {
 
-	id := util.SnowflakeID()
+	id, idString := util.SnowflakeID()
 	fmt.Print(id)
-	c.JSON(200, id)
+	fmt.Print(idString)
+	c.JSON(200, idString)
 	return
 }
