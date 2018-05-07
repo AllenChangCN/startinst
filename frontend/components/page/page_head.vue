80040bbdf4e6d69a55ff52c304b31dcc6f41b6fe
liuyuancheng <tenstone@foxmail.com>
2018-05-07 14:09:54 +0800

    dial button 改成 置顶的下拉内容

--- frontend/components/page/page_head.vue
+++ frontend/components/page/page_head.vue
@@ -7,9 +7,10 @@
         >
         <div slot="header" id="title">
           <div style="display: flex;">
-              <div style="flex: 100%;">
-                <b><v-icon>title</v-icon> / 王的导航</b>
-              </div>
+            <div style="flex: 100%;">
+              <b><v-icon>title</v-icon> / 王的导航</b>
+            </div>
+            <div>1,2,3</div>
             <v-tooltip bottom>
               <v-icon slot="activator" small @click.stop="settingsClick()" color="blue">settings</v-icon>
               <span>页面设置</span>
@@ -23,7 +24,8 @@
         <v-card>
           <v-card-text class="grey lighten-3">
             困难像弹簧，你强他就弱，你弱他就强。<br>
-            页面描述、浏览量、收藏量信息
+            页面描述、浏览量、收藏量信息 <br>
+            帮助 <br>
           </v-card-text>
         </v-card>
       </v-expansion-panel-content>
