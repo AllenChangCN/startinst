474420bc22e983ee5a5d3de8299a067800a6c28b
liuyuancheng <tenstone@foxmail.com>
2018-05-08 18:09:44 +0800

    开始处理Widget的排序

--- frontend/components/page/show_page.vue
+++ frontend/components/page/show_page.vue
@@ -12,12 +12,13 @@
         <page_head/>
       </v-flex>
       <!--遍历列-->
-      <v-flex lg3 v-for="widget_column in widgets" :key="widget_column.column_idx"
-              @mouseenter="columnEnter(widget_column.column_idx)"
-              @mouseleave="columnLeave()"
-      >
+      <v-flex lg3 v-for="widget_column in widgets" :key="widget_column.column_idx">
         <!--遍历Widget-->
-        <draggable v-model="widget_column.content" @start="drag=true" @end="drag=false">
+        <draggable v-model="widgets"
+                   :list="widget_column.data"
+                   :options="{group:''}"
+                   @start="drag=true"
+                   @end="drag=false">
           <div v-for="widget in widget_column.data" :key="widget.idx" style="margin-bottom: 13px;">
             <widget :data="widget" :column_idx="widget_column.column_idx"/>
           </div>
@@ -78,21 +79,27 @@
             name:'',
             data:[
               {idx: 1, title: 'test1',type:'ad',description:'先保存在终端，然后再统一上传，减少并发',},
-              {idx: 2, title: '岁吧点点', description:'',type:'bookmark', content:[
-                {idx:1,icon:'',title:'百度搜索引擎',url:'https://www.baidu.com'},
-                {idx:2,icon:'',title:'谷歌搜索引擎',url:'https://www.baidu.com'},
-                ]},
-              {idx: 2, title: 'test2',description:'描述2',type:'bookmark',content:[
-                {idx:1, icon:'',title: '腾讯网',url:'http://www.qq.com'},
-                {idx:2,icon:'',title: '腾讯网2',url:'http://www.qq.com'},
-                {idx:3,icon:'',title: '腾讯网3',url:'http://www.qq.com'},
-                ]},
+              {idx: 2, title: '岁吧点点', description:'',type:'bookmark',
+                content:[
+                  {idx:1,icon:'',title:'百度搜索引擎',url:'https://www.baidu.com'},
+                  {idx:2,icon:'',title:'谷歌搜索引擎',url:'https://www.baidu.com'},
+                ]
+              },
+              {idx: 3, title: 'test2',description:'描述2',type:'bookmark',
+                content:[
+                  {idx:1, icon:'',title: '腾讯网',url:'http://www.qq.com'},
+                  {idx:2,icon:'',title: '腾讯网2',url:'http://www.qq.com'},
+                  {idx:3,icon:'',title: '腾讯网3',url:'http://www.qq.com'},
+                ]
+              },
             ]},
           {
             column_idx:2,
             name:'',
             data: [
-              {idx: 2, title: 'test2',type:'note',description:'描述',content:'<h2>一去二三里</h2>烟村四五家，亭台六七座<br/>支持MarkDown'},
+              {idx: 2, title: 'test2',type:'note',description:'描述',
+                content:'<h2>一去二三里</h2>烟村四五家，亭台六七座<br/>支持MarkDown'
+              },
               {idx: 3, title: 'test2',type:'bookmark',description:'描述',}
             ]},
           {
